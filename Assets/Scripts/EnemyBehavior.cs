c49d7f2537603d5e968861610148c4992edc7ce5
jilikuo <joaovitorbariri@hotmail.com>
2024-05-29 08:47:39 -0300

    Check-in inicial do módulo top-down

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    public float move_speed;
+    public GameObject target_object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        target_object = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+
+    }
+
+    void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target_object.transform.position, move_speed);
+    }
+}
