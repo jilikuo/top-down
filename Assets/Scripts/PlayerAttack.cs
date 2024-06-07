db55f1ceac7735dc9f9cafc9d1d24844fc3fb749
jilikuo <54220633+jilikuo@users.noreply.github.com>
2024-06-07 12:00:26 -0300

    construcao do sistema de ataque
    criacao e instaciacao do projetil, script para dano de projetil e alteracoes no prefab de projetil

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public GameObject projetil;
+    EntityStats entity_stats;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        entity_stats = gameObject.GetComponent<EntityStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonUp(0))
         {
+            GameObject projectile_instance = Instantiate(projetil, transform.position, Quaternion.identity);
 
+            projectile_instance.GetComponent<ProjectileDamage>().projectile_damage = entity_stats.attack_damage;
+
+            Vector2 projectile_direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            projectile_direction.Normalize();
+
+            projectile_instance.GetComponent<Rigidbody2D>().AddForce(projectile_direction * 10, ForceMode2D.Impulse);
         }
     }
 }
